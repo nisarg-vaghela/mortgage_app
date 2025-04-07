8d10ff77ba26eb3875a8ff9bef34aa0b01fba3c5
nisarg.vaghela <nisarg.vaghela@e.lloydsbanking.com>
2025-04-07 12:14:04 +0000

    delete node modules added accidently

--- mortgage-ui/src/components/Login.tsx
+++ mortgage-ui/src/components/Login.tsx
@@ -19,7 +19,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
-        <div className="col-md-6">
+        <div className="col-md-12">
           <div className="card">
             <div className="card-body">
               <h2 className="card-title text-center">Login</h2>
